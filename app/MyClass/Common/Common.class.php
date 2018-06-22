0e6df56d0ccad896e934d3a40a39ae4d46455f80
学校-PeopleSea <1642216889@qq.com>
2018-06-22 09:45:55 +0800

    同步

--- app/MyClass/Common/Common.class.php
+++ app/MyClass/Common/Common.class.php
@@ -27,7 +27,7 @@ class Common{
 	}
 
     public static function create_user_token($user){
-        $token = md5(time().$user->credentials.$user->login_uuid);
+        $token = md5(time().$user->account);
         if($user->update(['token'=>$token])){
             return $token;
         }else{
